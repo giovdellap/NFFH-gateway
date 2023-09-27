b843bfbbb762466255023d492eed02410189ea7e
giovdellap <giovdellap@gmail.com>
2023-09-27 19:21:36 +0200

    Has to be fixed

---
+++ src/main/java/com/nffh/apigateway/ClientBEConfiguration.java
@@ -0,0 +1,34 @@
+package com.nffh.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientBEConfiguration {
+    
+    @Value("${clientBE.url}")
+    private String url;
+
+    @Value("${clientBE.port}")
+    private String port;
+
+    @Value("{genericFE.url}")
+    private String genericFE;
+
+    @Bean
+    public RouteLocator clientBELocator(RouteLocatorBuilder builder) {
+
+        String clientBEurl = this.url+":"+this.port;
+        return builder.routes()
+            // ADMIN
+            .route("getProfile", r -> r.path("/admin/client")
+                .uri(clientBEurl))
+            .route("search", r -> r.path("/admin/client")
+                .uri(clientBEurl))
+            
+        .build();
+    }
+}
