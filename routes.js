a338858a034032c2a6f0850c3b755af40fda1215
giovdellap <giovdellap@gmail.com>
2023-10-10 22:28:59 +0200

    mini commit

--- routes.js
+++ routes.js
@@ -8,7 +8,20 @@ const ROUTES = [
             max: 5
         },
         proxy: {
-            target: "http://client-be:8081",
+            target: "http://client-be:9702",
+            changeOrigin: false
+        }
+    },
+    {
+        url: '/provaclient',
+        auth: false,
+        creditCheck: false,
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 5
+        },
+        proxy: {
+            target: "http://client-be:9702",
             changeOrigin: false
         }
     },
@@ -47,7 +60,8 @@ const ROUTES = [
             max: 5
         },
         proxy: {
-            target: "http://auth:9701",
+            target: "http://client-be:9702",
+            //target: "http://auth:9701",
             changeOrigin: false
         }
     }
