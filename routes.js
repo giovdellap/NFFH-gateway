f97c6f54f70b07eb79a4f93034a8620c4520aff2
giovdellap <giovdellap@gmail.com>
2023-10-10 14:07:27 +0200

    Minor fix

--- routes.js
+++ routes.js
@@ -21,7 +21,33 @@ const ROUTES = [
             max: 5
         },
         proxy: {
-            target: "http://auth:9702",
+            target: "http://auth:9701",
+            changeOrigin: false
+        }
+    },
+    {
+        url: '/login',
+        auth: false,
+        creditCheck: false,
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 5
+        },
+        proxy: {
+            target: "http://auth:9701",
+            changeOrigin: false
+        }
+    },
+    {
+        url: '/signup',
+        auth: false,
+        creditCheck: false,
+        rateLimit: {
+            windowMs: 15 * 60 * 1000,
+            max: 5
+        },
+        proxy: {
+            target: "http://auth:9701",
             changeOrigin: false
         }
     }
