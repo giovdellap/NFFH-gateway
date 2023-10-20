2089e13c90b49f717881ade0141ae2acbbdd25ce
alessandropisent <alessandro.pisent@hotmail.com>
2023-10-20 14:50:23 +0200

    FUNZIONA con ImageServer (alpine)

--- routes.js
+++ routes.js
@@ -2,13 +2,14 @@ const { ROUTES_DEBUG } = require("./debug-routes");
 const { ROUTES_AUTH } = require("./routes-auth");
 const { ROUTES_CLIENT } = require("./routes-client");
 const { ROUTES_FARMER } = require("./routes-farmer");
+const { ROUTES_IMAGE } = require("./routes-image");
 
 const ROUTES = [
     ...ROUTES_AUTH,
     //...ROUTES_DEBUG,
     ...ROUTES_CLIENT,
-    ...ROUTES_FARMER
-
+    ...ROUTES_FARMER,
+    ...ROUTES_IMAGE
 ]
 
 exports.ROUTES = ROUTES;
