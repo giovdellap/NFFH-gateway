e35267f22287ec2affa995a628cf6dfbfba5cb28
giovdellap <giovdellap@gmail.com>
2023-10-21 21:07:39 +0200

    Main upgrade

---
+++ src/conf.js
@@ -0,0 +1,4 @@
+export const AUTH = "http://auth:9701";
+export const CLIENT_BE = "http://client-be:9702";
+export const FARMER_BE = "http://farmer-be:9703";
+export const IMAGE = "http://imageServer:9705";
